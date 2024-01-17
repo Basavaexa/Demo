766cd4f4ac276d4229229d4faa8faa08184d606b
SuvenduB <SuvenduB@EC-HKKKTT2>
2024-01-17 18:19:59 +0530

    Commit P1 & P2 to SuvenduBranch

---
+++ src/test/java/DMO/P2.java
@@ -0,0 +1,16 @@
+package DMO;
+
+public class P2 {
+
+	public static void main(String[] args) 
+	{
+		
+			System.out.println("Hi");
+			System.out.println("Hello");
+			System.out.println("By");
+
+		
+
+	}
+
+}
